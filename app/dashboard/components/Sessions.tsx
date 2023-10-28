cc6524f5baf57f7b27d34a06e472ae5a5fb7eaab
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-10-28 19:48:48 -0400

    Fixed scroll view height on sessions

--- app/dashboard/components/Sessions.tsx
+++ app/dashboard/components/Sessions.tsx
@@ -1,28 +1,79 @@
 import { Button } from "@nextui-org/react";
 import SessionCard from "./Cards/SessionCard";
 import { useRouter } from "next/navigation";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 export default function Sessions() {
 
-    const data = {
-        date: "Random date"
-    }
+    const data = [
+        {
+            summary: "Frequent headaches, mild fever, and fatigue",
+            date: "10-22-21",
+            flags: [
+                "Headaches",
+                "Fatigue",
+            ]
+        },
+        {
+            summary: "Severe back pain and muscle stiffness",
+            date: "10-23-21",
+            flags: [
+                "Back pain",
+                "Muscle stiffness",
+            ]
+        },
+        {
+            summary: "Allergies, runny nose, and cough",
+            date: "10-24-21",
+            flags: [
+                "Allergies",
+                "Cough",
+            ]
+        },
+        {
+            summary: "Sore throat, high temperature, and fatigue",
+            date: "10-25-21",
+            flags: [
+                "Sore throat",
+                "Fever",
+                "Fatigue",
+            ]
+        },
+        {
+            summary: "Joint pain and inflammation",
+            date: "10-26-21",
+            flags: [
+                "Joint pain",
+                "Inflammation",
+            ]
+        },
+        {
+            summary: "Nausea, vomiting, and stomach cramps",
+            date: "10-27-21",
+            flags: [
+                "Nausea",
+                "Vomiting",
+                "Stomach cramps",
+            ]
+        },
+    ];
 
     const router = useRouter();
 
     return (
-        <div>
+        <div className="flex flex-col h-screen">
             <div className="flex flex-row items-center mb-[20px]">
                 <p className="text-2xl">Sessions</p>
                 <div className="flex-1"></div>
                 <Button onClick={() => router.push(`/dashboard/session/new?id=1234`)}>Create Session</Button>
             </div>
-
-            <SessionCard props={data} />
-            <SessionCard props={data} />
-            <SessionCard props={data} />
-            <SessionCard props={data} />
-            <SessionCard props={data} />
+            <div className="overflow-y-auto pb-[75px]">
+                {data.map((session) => {
+                    return (
+                        <SessionCard props={session} />
+                    )
+                })}
+            </div>
         </div>
 
     )
