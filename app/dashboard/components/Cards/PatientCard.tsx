159a4929092ef7b26f6ce9e5cd9ad9e9092b53b7
Farouk Charkas <farouk.charkas@gmail.com>
2023-10-28 19:19:03 -0400

    Style the profile cards

--- app/dashboard/components/Cards/PatientCard.tsx
+++ app/dashboard/components/Cards/PatientCard.tsx
@@ -1,12 +1,15 @@
-'use client';
+"use client";
 
+import { Icon } from "@iconify/react/dist/iconify.js";
 import { ListboxItem, ListboxSection } from "@nextui-org/react";
 
 export default function PatientCard({ props }: any) {
-    return (
-        <div>
-            {props.name}
-        </div>
-    )
-
+  return (
+    <div className="flex items-center h-16 w-full border-1 border-solid border-indigo-700 rounded-2xl py-2 px-2 mb-2">
+      <div className="flex-1">
+        <p className="text-md">{props.name}</p>
+        <p className="text-xs line-clamp-1">{props.description}</p>
+      </div>
+    </div>
+  );
 }
