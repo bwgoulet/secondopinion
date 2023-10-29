aba3551d477e76023925d5f7648317f2a42e3c3e
noah <90880131+noahsmiths@users.noreply.github.com>
2023-10-29 05:07:23 -0400

    Spinner on session list while no summary

--- app/dashboard/components/Cards/SessionCard.tsx
+++ app/dashboard/components/Cards/SessionCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Icon } from "@iconify/react/dist/iconify.js";
-import { Card, CardBody, CardFooter, Chip } from "@nextui-org/react";
+import { Card, CardBody, CardFooter, Chip, Spinner } from "@nextui-org/react";
 import { format } from "date-fns";
 
 export default function SessionCard({ session, setSession }: any) {
@@ -15,7 +15,7 @@ export default function SessionCard({ session, setSession }: any) {
                 <p className="text-xl ">
                     {format(new Date(session.timestamp), "MM/dd/yyyy")}
                 </p>
-                <p className="text-sm line-clamp-3">{session.summary}</p>
+                <p className="text-sm line-clamp-3">{session.summary || <Spinner />}</p>
             </CardBody>
             {/* </CardFooter> */}
         </Card>
