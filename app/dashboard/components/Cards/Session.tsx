a2684460f2923b1cb031ead8529ad253f53c9825
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-10-29 02:23:24 -0400

    CHANGES

--- app/dashboard/components/Cards/Session.tsx
+++ app/dashboard/components/Cards/Session.tsx
@@ -1,51 +1,59 @@
 "use client";
 
-import { Button, Card, CardBody, CardHeader, Chip } from "@nextui-org/react";
+import { Accordion, AccordionItem, Button, Card, CardBody, CardHeader, Chip, ScrollShadow } from "@nextui-org/react";
 import SessionAccordion from "../SessionAccordion";
+import format from 'date-fns/format'
 
-export default function Session({ session, selectedPatient }) {
+export default function Session({ session, selectedPatient }: any) {
+
+  function dateFormatter(timestamp: any) {
+    var date = new Date(timestamp);
+
+    // Extract the date, month, and year
+    var month = date.getMonth() + 1; // Months are 0-based
+    var day = date.getDate();
+    var year = date.getFullYear();
+
+    // Format the date as MM/dd/yyyy
+    var formattedDate = month + '/' + day + '/' + year;
+
+    return formattedDate
+  }
+
+
   return (
-    <div>
+    <div className="">
       <div className="px-2">
         <div className="flex h-[60px] items-center mb-[20px]">
-          <p className=" text-2xl flex-1">{selectedPatient?.name}'s Session (XX/XX/XX)</p>
+          <p className=" text-2xl flex-1">{selectedPatient?.name}'s {dateFormatter(session?.timestamp)}</p>
           <Button>Close</Button>
+        </div>
+        <div className="flex flex-col mb-5">
+          <p className="font-bold text-sm mb-2">Flags</p>
+          <div className="gap-4">
+            {session?.flags?.map((flag) => {
+              return (
+                <div>
+                  <Chip color="danger" variant="flat" className="mr-1 mb-1">
+                    {flag.issue}
+                  </Chip>
+                  <Chip color="danger" variant="flat" className="mr-1 mb-1">
+                    {flag.issue}
+                  </Chip>
+                </div>
+              )
+            })
+            }
+          </div>
         </div>
         <p className="font-bold text-sm">Summary</p>
         <p className="mb-5">{session?.summary}</p>
-        <div className="flex mb-5">
-          <div className="flex-1">
-            <p className="font-bold text-sm">Danger Level</p>
-            <div className="flex flex-col bg-red-600 h-[100px] p-5 rounded-lg">
-              <div className="flex-1"></div>
-              <p className="text-white text-xl">Dangerous</p>
-            </div>
-          </div>
-          <div className="w-[20px]"></div>
-          <div className="flex-1">
-            <p className="font-bold text-sm">Flags</p>
-            <div className="gap-4">
-              <Chip color="danger" variant="flat" className="mr-1 mb-1">
-                Dangerous Dose
-              </Chip>
-              <Chip color="danger" variant="flat" className="mr-1 mb-1">
-                Dangerous For Age
-              </Chip>
-              <Chip color="warning" variant="flat" className="mr-1 mb-1">
-                No Mention of Side Effects
-              </Chip>
-              <Chip color="success" variant="flat" className="mr-1 mb-1">
-                Great Communication
-              </Chip>
-              <Chip color="danger" variant="flat" className="mr-1 mb-1">
-                Potential Interaction
-              </Chip>
-            </div>
-          </div>
-        </div>
       </div>
-      <div>
-        <SessionAccordion></SessionAccordion>
+      <div className="px-2">
+        <p className="font-bold">Transcript</p>
+        <ScrollShadow className="w-[800px] h-[400px] whitespace-pre">
+          {session?.transcript}
+        </ScrollShadow>
       </div>
     </div>
   );
