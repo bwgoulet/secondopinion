a74b013aa74843ead2a27919369a513569c3ff86
Ben Goulet <bwgoulet@ad.unc.edu>
2023-10-29 02:21:56 -0400

    clean up sessions - disable session creation without patient selected, UI update for session card, etc

--- app/dashboard/components/Sessions.tsx
+++ app/dashboard/components/Sessions.tsx
@@ -4,31 +4,41 @@ import { useRouter } from "next/navigation";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { useSession } from "next-auth/react";
 
-export default function Sessions({ selectedPatient, setPatient, sessions, setSession }: any) {
+export default function Sessions({
+  selectedPatient,
+  setPatient,
+  sessions,
+  setSession,
+}: any) {
+  const session = useSession();
+  const router = useRouter();
 
-    const session = useSession();
-    const router = useRouter();
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="flex lg:flex-row sm:flex-col md:flex-col xs:flex-col items-center mb-[20px] h-[60px]">
+        <p className="text-2xl">Sessions</p>
+        <div className="flex-1"></div>
+        <Button
+          isDisabled={!selectedPatient?._id}
+          className="bg-indigo-600 text-white h-[40px]"
+          onClick={() =>
+            router.push(`/dashboard/session/new?id=${selectedPatient?._id}`)
+          }
+        >
+          Create Session
+        </Button>
+      </div>
+      <div className="overflow-y-auto pb-[75px]">
+        {!selectedPatient?._id && <p>Please select a patient!</p>}
 
-    return (
-        <div className="flex flex-col h-screen">
-            <div className="flex lg:flex-row sm:flex-col md:flex-col xs:flex-col items-center mb-[20px] h-[60px]">
-                <p className="text-2xl">Sessions</p>
-                <div className="flex-1"></div>
-                <Button
-                    className="bg-indigo-600 text-white h-[40px]"
-                    onClick={() => router.push(`/dashboard/session/new?id=${selectedPatient?._id}`)}
-                >
-                    Create Session
-                </Button>
-            </div>
-            <div className="overflow-y-auto pb-[75px]">
-                {!selectedPatient && <p>Please select a patient</p>}
+        {!sessions[0] && selectedPatient?._id && (
+          <p>No sessions for {selectedPatient.name}!</p>
+        )}
 
-                {sessions.map((session: any) => {
-                    return <SessionCard session={session} setSession={setSession} />
-                })}
-
-            </div>
-        </div>
-    );
+        {sessions.map((session: any) => {
+          return <SessionCard session={session} setSession={setSession} />;
+        })}
+      </div>
+    </div>
+  );
 }
