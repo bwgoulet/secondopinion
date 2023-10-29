5d0265e19ece6c62e35bc9294b23f5d6c3ae551c
Farouk Charkas <farouk.charkas@gmail.com>
2023-10-28 20:06:12 -0400

    Styled side nav

--- app/dashboard/components/Cards/PatientCard.tsx
+++ app/dashboard/components/Cards/PatientCard.tsx
@@ -1,15 +1,23 @@
 "use client";
 
 import { Icon } from "@iconify/react/dist/iconify.js";
-import { ListboxItem, ListboxSection } from "@nextui-org/react";
+import { Card, CardBody, ListboxItem, ListboxSection } from "@nextui-org/react";
 
 export default function PatientCard({ props }: any) {
   return (
-    <div className="flex items-center h-16 w-full border-1 border-solid border-indigo-700 rounded-2xl py-2 px-2 mb-2">
-      <div className="flex-1">
-        <p className="text-md">{props.name}</p>
-        <p className="text-xs line-clamp-1">{props.description}</p>
-      </div>
-    </div>
+    <Card isPressable className="bg-indigo-600 text-indigo-50 mb-2">
+      <CardBody className="flex flex-row items-center">
+        <div className="pr-2">
+          <Icon icon="bx:user" width="22"></Icon>
+        </div>
+        <div className="flex-1">
+          <p className="text-md">{props.name}</p>
+          <p className="text-xs line-clamp-1">{props.description}</p>
+        </div>
+      </CardBody>
+    </Card>
+    // <div className="flex items-center h-16 w-full border-1 border-solid border-indigo-700 rounded-2xl py-2 px-2 mb-2 hover:border-indigo-800">
+
+    // </div>
   );
 }
--- app/dashboard/components/SideNav.tsx
+++ app/dashboard/components/SideNav.tsx
@@ -1,64 +1,73 @@
-'use client';
+"use client";
 
-import { Avatar, Button, Divider, Listbox, ListboxItem, ListboxSection } from "@nextui-org/react";
+import {
+  Avatar,
+  Button,
+  Divider,
+  Listbox,
+  ListboxItem,
+  ListboxSection,
+} from "@nextui-org/react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { Icon } from '@iconify/react';
+import { Icon } from "@iconify/react";
 import PatientCard from "./Cards/PatientCard";
 
-
 export default function SideNav() {
+  const session = useSession();
+  const router = useRouter();
 
-    const session = useSession();
-    const router = useRouter();
+  const data = [
+    {
+      name: "Emerson",
+      description: "Male, 20 years old, heart disease",
+    },
+    {
+      name: "Johanna",
+      description: "Female, 19 years old, blah blah",
+    },
+    {
+      name: "Fredrick",
+      description: "Male, 15 years old, blah blah",
+    },
+    {
+      name: "Ryan",
+      description: "Male, 15 years old, blah blah",
+    },
+  ];
 
-    const data = [
-        {
-            name: "Emerson",
-            description: "Male, 20 years old, heart disease",
-        },
-        {
-            name: "Johanna",
-            description: "Female, 19 years old, blah blah",
-        },
-        {
-            name: "Fredrick",
-            description: "Male, 15 years old, blah blah",
-        },
-        {
-            name: "Ryan",
-            description: "Male, 15 years old, blah blah",
-        },
-
-    ]
-
-    return (
-        <aside className="h-screen text-white w-60">
-            <nav className="h-full flex flex-col bg-indigo-600 border-r border-indigo-300 shadow-sm">
-                <div className="flex justify-between items-center p-[20px]">
-                    <h1 className="text-2xl">Patients</h1>
-                </div>
-                <div className="flex-1 max-w-[260px] h-fit rounded-small border-default-200 dark:border-default-100">
-                    <Button variant="flat" className="p-4">Add new patient</Button>
-                    <div className="flex flex-col p-3">
-                        {data.map((patient) => {
-                            return (
-                                <PatientCard props={patient} />
-                            )
-                        })}
-                    </div>
-                    {/* </Listbox> */}
-                </div>
-                <div className="flex-1"></div>
-                <div className="border-t border-zinc-800 flex p-3">
-                    <Avatar isBordered color="success" size="sm" src={session?.data?.user?.image ?? ""} />
-                    <div className="flex justify-between items-center ml-3">
-                        <div className="leading-4">
-                            <h4 className="font-semibold ">{session?.data?.user?.name}</h4>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </aside >
-    )
+  return (
+    <aside className="h-screen text-white w-60">
+      <nav className="h-full flex flex-col bg-indigo-600 border-r border-indigo-300 shadow-sm">
+        <div className="flex justify-between items-center px-[20px] pt-[20px]">
+          <h1 className="text-2xl">Patients</h1>
+        </div>
+        <div className="flex-1 max-w-[260px] h-fit rounded-small border-default-200 dark:border-default-100">
+          <div className="p-3">
+            <Button className="p-4 w-full">Add new patient</Button>
+          </div>
+          <div className="flex flex-col p-3">
+            {data.map((patient) => {
+              return <PatientCard props={patient} />;
+            })}
+          </div>
+          {/* </Listbox> */}
+        </div>
+        <div className="flex-1"></div>
+        <div className="border-t border-zinc-800 flex p-3">
+          <Avatar
+            isBordered
+            color="success"
+            size="sm"
+            src={session?.data?.user?.image ?? ""}
+          />
+          <div className="flex justify-between items-center ml-3">
+            <div className="leading-4">
+              <h4 className="font-semibold ">{session?.data?.user?.name}</h4>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </aside>
+  );
 }
