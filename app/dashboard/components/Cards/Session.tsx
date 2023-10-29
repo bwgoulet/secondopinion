182176a7e228898b3d8b7863d623c853bb200404
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-10-29 02:42:21 -0400

    Session

--- app/dashboard/components/Cards/Session.tsx
+++ app/dashboard/components/Cards/Session.tsx
@@ -22,39 +22,41 @@ export default function Session({ session, selectedPatient }: any) {
 
 
   return (
-    <div className="">
-      <div className="px-2">
-        <div className="flex h-[60px] items-center mb-[20px]">
-          <p className=" text-2xl flex-1">{selectedPatient?.name}'s {dateFormatter(session?.timestamp)}</p>
-          <Button>Close</Button>
-        </div>
-        <div className="flex flex-col mb-5">
-          <p className="font-bold text-sm mb-2">Flags</p>
-          <div className="gap-4">
-            {session?.flags?.map((flag) => {
-              return (
-                <div>
-                  <Chip color="danger" variant="flat" className="mr-1 mb-1">
-                    {flag.issue}
-                  </Chip>
-                  <Chip color="danger" variant="flat" className="mr-1 mb-1">
-                    {flag.issue}
-                  </Chip>
-                </div>
-              )
-            })
-            }
+    <div>
+      {!session.timestamp ? <p></p>
+        :
+        <div>
+          <div className="px-2">
+            <div className="flex h-[60px] items-center mb-[20px]">
+              <p className=" text-2xl flex-1">{selectedPatient?.name}'s {dateFormatter(session?.timestamp)}</p>
+              {/* <Button>Close</Button> */}
+            </div>
+            <div className="flex flex-col mb-5">
+              <p className="font-bold text-sm mb-2">Flags</p>
+              <div className="gap-4">
+                {session?.flags?.map((flag) => {
+                  return (
+                    <div>
+                      <Chip color="danger" variant="flat" className="mr-1 mb-1">
+                        {flag.issue}
+                      </Chip>
+                    </div>
+                  )
+                })
+                }
+              </div>
+            </div>
+            <p className="font-bold text-sm">Summary</p>
+            <p className="mb-5">{session?.summary}</p>
+          </div>
+          <div className="px-2">
+            <p className="font-bold">Transcript</p>
+            <ScrollShadow className="w-[800px] h-[400px] whitespace-pre">
+              {session?.transcript}
+            </ScrollShadow>
           </div>
         </div>
-        <p className="font-bold text-sm">Summary</p>
-        <p className="mb-5">{session?.summary}</p>
-      </div>
-      <div className="px-2">
-        <p className="font-bold">Transcript</p>
-        <ScrollShadow className="w-[800px] h-[400px] whitespace-pre">
-          {session?.transcript}
-        </ScrollShadow>
-      </div>
-    </div>
+      }
+    </div >
   );
 }
