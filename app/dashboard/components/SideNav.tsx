e478e6bc6af27ef854b7c768ac447f8702a21dee
Farouk Charkas <farouk.charkas@gmail.com>
2023-10-28 21:42:51 -0400

    Style some things

--- app/dashboard/components/SideNav.tsx
+++ app/dashboard/components/SideNav.tsx
@@ -39,12 +39,12 @@ export default function SideNav() {
   return (
     <aside className="h-screen text-white w-60">
       <nav className="h-full flex flex-col bg-indigo-600 border-r border-indigo-300 shadow-sm">
-        <div className="flex justify-between items-center px-[20px] pt-[20px]">
+        <div className="flex justify-between items-center px-[20px] pt-[20px] h-[60px]">
           <h1 className="text-2xl">Patients</h1>
         </div>
         <div className="flex-1 max-w-[260px] h-fit rounded-small border-default-200 dark:border-default-100">
           <div className="p-3">
-            <Button className="p-4 w-full">Add new patient</Button>
+            <Button className="p-4 w-full bg-white">Add new patient</Button>
           </div>
           <div className="flex flex-col p-3">
             {data.map((patient) => {
